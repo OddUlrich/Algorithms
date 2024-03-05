64961a69820fbc9176b081bb4a4e3d51ce1653fb
OddUlrich <vmoonodd@gmail.com>
2024-03-05 22:11:09 +0800

    DFS in Binary tree on Solution 144.

---
+++ Leetcode/DFS/Solution144_2.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution144_2 {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (root == null) {
+            return res;
+        }
+
+        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+        TreeNode node = root;
+        while (!stack.isEmpty() || node != null) {
+            while (node != null) {
+                res.add(node.val);
+                stack.push(node);
+                node = node.left;
+            }
+            node = stack.pop();
+            node = node.right;
+        }
+        return res;
+    }
+
+}
